cf3666ba37e445a16c994853b69b245a6dce1942
Admin <admin@armely.com>
2026-04-02 06:10:48 -0500

    vvvv

--- store/app/Providers/RouteServiceProvider.php
+++ store/app/Providers/RouteServiceProvider.php
@@ -33,11 +33,6 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            // Support deployments where the store app is mounted under /store.
-            Route::middleware('api')
-                ->prefix('store/api')
-                ->group(base_path('routes/api.php'));
-
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
