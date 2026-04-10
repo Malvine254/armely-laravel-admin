e840104e8460cf32bbd0e8b3efd818a486acf726
Admin <admin@armely.com>
2026-04-10 12:02:28 -0500

    bbbb

--- store/config/filesystems.php
+++ store/config/filesystems.php
@@ -41,7 +41,13 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => env(
+                'PUBLIC_STORAGE_URL',
+                rtrim(
+                    env('ASSET_URL', env('FRONTEND_URL', env('APP_URL', 'http://localhost'))),
+                    '/'
+                ) . '/storage'
+            ),
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
