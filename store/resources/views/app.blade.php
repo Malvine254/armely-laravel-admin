bfd02499e28b910ee827604541775e2290290906
Admin <admin@armely.com>
2026-04-22 16:09:20 -0500

    xxxx

--- store/resources/views/app.blade.php
+++ store/resources/views/app.blade.php
@@ -8,6 +8,10 @@
 
         <title>{{ config('app.name', 'Armely Store') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/png" href="/images/logo/armely-store-logo.png">
+        <link rel="shortcut icon" type="image/png" href="/images/logo/armely-store-logo.png">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
