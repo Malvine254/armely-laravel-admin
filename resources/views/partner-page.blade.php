348c6f0938e2bbeb66552c4674aba9415e4c986e
melo254 <owuormalvine75@gmail.com>
2026-04-27 11:54:56 -0500

    xxxx

--- resources/views/partner-page.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.public')
-
-@section('title', isset($pageTitle) ? $pageTitle . ' - Armely' : 'Partner - Armely')
-
-@section('content')
-    <div class="partner-page-wrapper">
-        {!! $content !!}
-    </div>
-@endsection
