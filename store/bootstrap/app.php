fd6cb0d4873c8fb1fa264d2b58c38c45b3b4518a
melo254 <owuormalvine75@gmail.com>
2026-05-29 12:00:18 -0500

    saved

--- store/bootstrap/app.php
+++ store/bootstrap/app.php
@@ -13,8 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\KickOffDuePriceSync::class);
-
         $middleware->alias([
             'active.user' => \App\Http\Middleware\EnsureUserIsActive::class,
         ]);
