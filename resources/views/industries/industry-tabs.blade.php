d40075a4b4786be3b4d0a5984f5ef0ca000a5ecd
melo254 <owuormalvine75@gmail.com>
2026-08-04 13:40:53 -0500

    sorted

--- resources/views/industries/industry-tabs.blade.php
+++ resources/views/industries/industry-tabs.blade.php
@@ -1,56 +1,48 @@
 @php
-	$industryTabIcons = [
-		'healthcare' => 'icofont-doctor',
-		'energy' => 'icofont-fire-burn',
-		'financial-services' => 'icofont-bank-alt',
-		'higher-education' => 'icofont-graduate',
-		'manufacturing' => 'icofont-industry',
-		'nonprofit-social-services' => 'icofont-users-alt-1',
-		'professional-services' => 'icofont-briefcase',
-		'state-local-government' => 'icofont-building-alt',
-		'transportation-logistics' => 'icofont-delivery-time',
-		'agriculture-cannabis' => 'icofont-leaf',
-	];
+    $industryTabIcons = [
+        'healthcare' => 'icofont-doctor',
+        'energy' => 'icofont-fire-burn',
+        'financial-services' => 'icofont-bank-alt',
+        'higher-education' => 'icofont-graduate',
+        'manufacturing' => 'icofont-industry',
+        'nonprofit-social-services' => 'icofont-users-alt-1',
+        'professional-services' => 'icofont-briefcase',
+        'state-local-government' => 'icofont-building-alt',
+        'transportation-logistics' => 'icofont-delivery-time',
+        'agriculture-cannabis' => 'icofont-leaf',
+    ];
 
-	$primaryIndustries = array_slice($industryPages, 0, 5, true);
-	$secondaryIndustries = array_slice($industryPages, 5, null, true);
+    $primaryIndustries = array_slice($industryPages, 0, 5, true);
+    $secondaryIndustries = array_slice($industryPages, 5, null, true);
 @endphp
 
-<div class="industry-pages-tabs">
-	<div class="industry-pages-tabs-inner">
-		<ul class="nav nav-tabs modern-tabs-industries industry-pages-tablist" id="industryPagesTab" role="tablist">
-			@foreach($primaryIndustries as $slug => $page)
-				<li class="nav-item" role="presentation">
-					<a class="nav-link {{ $loop->first ? 'active' : '' }}" id="{{ $slug }}-tab" data-toggle="tab" href="#{{ $slug }}" role="tab" aria-controls="{{ $slug }}" aria-selected="{{ $loop->first ? 'true' : 'false' }}">
-						<i class="{{ $industryTabIcons[$slug] ?? 'icofont-building-alt' }}"></i>
-						<strong>{{ $page['label'] }}</strong>
-					</a>
-				</li>
-			@endforeach
-			@if(count($secondaryIndustries))
-				<li class="nav-item dropdown industry-more-dropdown" role="presentation">
-					<a class="nav-link dropdown-toggle industry-more-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-						<i class="icofont-ui-menu"></i>
-						<strong>Read More</strong>
-					</a>
-					<div class="dropdown-menu industry-more-menu">
-						@foreach($secondaryIndustries as $slug => $page)
-							<a class="dropdown-item industry-tab-link" href="#{{ $slug }}" data-target-tab="{{ $slug }}">
-								<i class="{{ $industryTabIcons[$slug] ?? 'icofont-building-alt' }}"></i>
-								<span>{{ $page['label'] }}</span>
-							</a>
-						@endforeach
-					</div>
-				</li>
-			@endif
-		</ul>
-
-		<div class="tab-content modern-tab-content industry-pages-tabcontent" id="industryPagesTabContent">
-			@foreach($industryPages as $slug => $page)
-				<div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="{{ $slug }}" role="tabpanel" aria-labelledby="{{ $slug }}-tab">
-					@include($page['view'])
-				</div>
-			@endforeach
-		</div>
-	</div>
-</div>
+<nav class="industry-pages-tabs" aria-label="Industry pages">
+    <div class="industry-pages-tabs-inner">
+        <ul class="nav nav-tabs modern-tabs-industries industry-pages-tablist">
+            @foreach($primaryIndustries as $slug => $page)
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('industries.show', $slug) }}">
+                        <i class="{{ $industryTabIcons[$slug] ?? 'icofont-building-alt' }}" aria-hidden="true"></i>
+                        <strong>{{ $page['label'] }}</strong>
+                    </a>
+                </li>
+            @endforeach
+            @if(count($secondaryIndustries))
+                <li class="nav-item dropdown industry-more-dropdown">
+                    <a class="nav-link dropdown-toggle industry-more-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                        <i class="icofont-ui-menu" aria-hidden="true"></i>
+                        <strong>More Industries</strong>
+                    </a>
+                    <div class="dropdown-menu industry-more-menu">
+                        @foreach($secondaryIndustries as $slug => $page)
+                            <a class="dropdown-item industry-tab-link" href="{{ route('industries.show', $slug) }}">
+                                <i class="{{ $industryTabIcons[$slug] ?? 'icofont-building-alt' }}" aria-hidden="true"></i>
+                                <span>{{ $page['label'] }}</span>
+                            </a>
+                        @endforeach
+                    </div>
+                </li>
+            @endif
+        </ul>
+    </div>
+</nav>
