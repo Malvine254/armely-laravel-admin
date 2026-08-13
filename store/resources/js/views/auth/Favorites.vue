64932f034d3907a75581c23b86d8d7349e52ee8b
melo254 <owuormalvine75@gmail.com>
2026-08-13 16:00:53 -0500

    Show three favorite cards per row on large screens

--- store/resources/js/views/auth/Favorites.vue
+++ store/resources/js/views/auth/Favorites.vue
@@ -39,7 +39,7 @@
         </button>
       </div>
 
-      <div v-else class="mb-8 grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div v-else class="mb-8 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <ProductCard
           v-for="(product, productIndex) in filteredFavorites"
           :key="product.productId"
